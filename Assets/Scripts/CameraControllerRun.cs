d8f16696c35fcf8be294d3db80034d7cf8f502c4
keiondesu96 <keiondesu96@gmail.com>
2022-07-14 17:36:12 +0400

    car rides & camera follows

---
+++ Assets/Scripts/CameraControllerRun.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerRun : MonoBehaviour
+{
+    private readonly Quaternion defaultRotation = new Quaternion(0.2f, 0.7f, -0.2f, 0.7f);
+    private readonly Vector3 defaultOffset = new Vector3(-7, 4, 0);
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        transform.rotation = defaultRotation;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ChasePlayerCar();
+    }
+
+    private void ChasePlayerCar()
+    {
+        var pCar = CarController.GetPlayerCar();
+
+        if (pCar != null)
+            transform.position = pCar.transform.position + defaultOffset;
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    static public List<CarController> allCars = new List<CarController>();
+
+    public bool IsPlayerCar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        allCars.Add(this);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MoveForward();
+    }
+
+    private void MoveForward()
+    {
+        transform.position += new Vector3(Time.deltaTime, 0, 0);
+    }
+
+    static public CarController GetPlayerCar()
+    {
+        foreach (var car in allCars)
+        {
+            if (car.IsPlayerCar)
+                return car;
+        }
+
+        return null;
+    }
+}
