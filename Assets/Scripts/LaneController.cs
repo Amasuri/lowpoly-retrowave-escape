2fddc6032580a1e1cab728319c933fe53c314d11
keiondesu96 <keiondesu96@gmail.com>
2022-07-14 20:19:26 +0400

    dynamic terrain spawning

--- Assets/Scripts/LaneController.cs
+++ Assets/Scripts/LaneController.cs
@@ -4,32 +4,49 @@ using UnityEngine;
 
 public class LaneController : MonoBehaviour
 {
+    private readonly Vector3 defaultTerrainPosition = new Vector3(-10, 0, -500);
+    private int TimesTerrainWasSpawned;
+
     public Transform TrafficCar1;
+    public Transform Terrain;
 
     public const float laneWidthInWU = 2f;
     public const float spawnOffsetX = 40f;
 
     //Timer
-    private const float maxSpawnDelay = 3f;
-    private float currentSpawnDelay;
+    private const float maxTrafficSpawnDelay = 3f;
+    private float currentTrafficSpawnDelay;
 
     static public bool HasPlayerCollided { get; private set; }
 
     // Start is called before the first frame update
     private void Start()
     {
-        currentSpawnDelay = maxSpawnDelay;
+        currentTrafficSpawnDelay = maxTrafficSpawnDelay;
+        TimesTerrainWasSpawned = 0;
+
+        SpawnNextTerrainChunk();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        currentSpawnDelay -= Time.deltaTime;
-        if(currentSpawnDelay <= 0f)
+        //Traffic spawning
+        currentTrafficSpawnDelay -= Time.deltaTime;
+        if(currentTrafficSpawnDelay <= 0f)
         {
-            currentSpawnDelay = maxSpawnDelay;
+            currentTrafficSpawnDelay = maxTrafficSpawnDelay;
             SpawnNewTraffic();
         }
+
+        //Terrain spawning
+        var pCar = CarController.GetPlayerCar();
+        if (pCar == null)
+            return;
+        var pPos = pCar.transform.position;
+
+        if (((pPos.x + 100f) / 1000f) > TimesTerrainWasSpawned)
+            SpawnNextTerrainChunk();
     }
 
     public static void RecordThatPlayerCollided()
@@ -47,6 +64,20 @@ public class LaneController : MonoBehaviour
 
         var newTrafficCar = Instantiate(TrafficCar1) as Transform;
         newTrafficCar.position = new Vector3(pPos.x + spawnOffsetX, pPos.y, GetRandomLane());
+    }
+
+    private void SpawnNextTerrainChunk()
+    {
+        var pCar = CarController.GetPlayerCar();
+        if (pCar == null)
+            return;
+
+        var pPos = pCar.transform.position;
+
+        var newTerrainChunk = Instantiate(Terrain) as Transform;
+        newTerrainChunk.position = defaultTerrainPosition + new Vector3(pPos.x, 0, 0);
+
+        TimesTerrainWasSpawned++;
     }
 
     private float GetRandomLane()
