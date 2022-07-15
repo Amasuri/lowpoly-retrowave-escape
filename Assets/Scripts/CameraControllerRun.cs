d1d08158dab41b13a8b5aebe7820e3a082bf5c32
keiondesu96 <keiondesu96@gmail.com>
2022-07-15 16:26:22 +0400

    changes angle depending on elapsed time

--- Assets/Scripts/CameraControllerRun.cs
+++ Assets/Scripts/CameraControllerRun.cs
@@ -5,31 +5,45 @@ using UnityEngine;
 
 public class CameraControllerRun : MonoBehaviour
 {
-    private readonly Quaternion defaultRotation = new Quaternion(0.2f, 0.7f, -0.2f, 0.7f); //~= degrees 27 90 0
+    private readonly Vector3 startRotVec = new Vector3(27, 90, 0);
+    private readonly Vector3 endRotVec = new Vector3(15, 90, 0);
+
     private readonly Vector3 defaultOffset = new Vector3(-7, 4, 0);
 
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log(transform.position);
-
-        transform.rotation = defaultRotation;
+        transform.rotation = Quaternion.Euler(startRotVec);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (LaneController.HasPlayerCollided)
+            return;
+
         ChasePlayerCar();
+        CorrectAngleBasedOnElapsedTime();
     }
 
     private void ChasePlayerCar()
     {
-        if (LaneController.HasPlayerCollided)
-            return;
-
         var pCar = CarController.GetPlayerCar();
 
         if (pCar != null)
             transform.position = pCar.transform.position + defaultOffset;
+    }
+
+    private void CorrectAngleBasedOnElapsedTime()
+    {
+        var rotLength = startRotVec.x - endRotVec.x; //wanted to make it a constant, but guess what? fields not supported
+
+        //Remaps elapsed time from time range 0-60s to camera degree range 0-12 degrees delta
+        var currentCameraRot = MathHelper.Remap(RunTimer.TimeSinceLastRunStartSec, 0f, 60f, 0f, rotLength);
+        var rotVec = startRotVec - new Vector3(currentCameraRot, 0, 0);
+        if (currentCameraRot >= rotLength)
+            rotVec = endRotVec;
+
+        transform.rotation = Quaternion.Euler(rotVec);
     }
 }
---
+++ Assets/Scripts/MathHelper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathHelper
+{
+    public static float Remap(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
+}
