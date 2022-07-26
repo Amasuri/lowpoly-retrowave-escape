717a4623acad81e9286ff86b233114c368ab5721
keiondesu96 <keiondesu96@gmail.com>
2022-07-26 21:46:42 +0400

    added delay before retry option

--- Assets/Scripts/GUI/RunButtonController.cs
+++ Assets/Scripts/GUI/RunButtonController.cs
@@ -6,16 +6,29 @@ public class RunButtonController : MonoBehaviour
 {
     static public RunButtonController current;
 
+    private bool retryButtonStartedCount;
+
+    private const float maxRetryDelaySec = 3f;
+    private float currentRetryDelaySec;
+
     // Start is called before the first frame update
     private void Start()
     {
         current = this;
+        currentRetryDelaySec = maxRetryDelaySec;
+        retryButtonStartedCount = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
         if (LaneController.HasPlayerCollided)
+            retryButtonStartedCount = true;
+
+        if (retryButtonStartedCount)
+            currentRetryDelaySec -= Time.deltaTime;
+
+        if(currentRetryDelaySec <= 0f)
             TurnOnRetryButton();
     }
 
@@ -23,6 +36,9 @@ public class RunButtonController : MonoBehaviour
     {
         var n = transform.Find("Retry Button");
         if (n == null)
+            return;
+
+        if (n.gameObject.active)
             return;
 
         n.gameObject.active = true;
