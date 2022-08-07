026790cbe55f1c51100aa80ed122ec9b77d5b364
keiondesu96 <keiondesu96@gmail.com>
2022-08-07 12:01:15 +0400

    button transparency action time tweaked

--- Assets/Scripts/GUI/ButtonFadeIn.cs
+++ Assets/Scripts/GUI/ButtonFadeIn.cs
@@ -24,7 +24,7 @@ public class ButtonFadeIn : MonoBehaviour
             return;
 
         thisImage.color = new Color(1, 1, 1, thisImage.color.a + (0.25f * Time.deltaTime));
-        if (thisImage.color.a >= 0.75f && !thisButton.enabled)
+        if (thisImage.color.a > 0f && !thisButton.enabled)
             thisButton.enabled = true;
     }
 }
