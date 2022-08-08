f0476535481b5c094b90799f9beb33cf32354286
keiondesu96 <keiondesu96@gmail.com>
2022-08-08 12:07:03 +0400

    new helper function: remap with limit

--- Assets/Scripts/MathHelper.cs
+++ Assets/Scripts/MathHelper.cs
@@ -18,4 +18,16 @@ public static class MathHelper
 
         return to;
     }
+
+    public static float RemapAndLimitToRange(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var num = Remap(from, fromMin, fromMax, toMin, toMax);
+
+        if (num > toMax)
+            num = toMax;
+        if (num < toMin)
+            num = toMin;
+
+        return num;
+    }
 }
