2be7260eafd9b1cc87c57b004507201d1daed82c
keiondesu96 <keiondesu96@gmail.com>
2022-09-18 21:11:30 +0400

    made a blinking timeline for blink effect on exhaust

--- Assets/Scripts/CarExhaustEffect.cs
+++ Assets/Scripts/CarExhaustEffect.cs
@@ -6,8 +6,15 @@ public class CarExhaustEffect : MonoBehaviour
 {
     public Light light;
 
-    private const float ExhaustCDmax = 0.1f;
+    private const float ExhaustCDmax = 0.3f / 2;
     private float ExhaustCDnow = 0f;
+
+    //private readonly Dictionary<float, bool> blinkTimeline = new Dictionary<float, bool>
+    //{
+    //    { 0.3f, false }, //0.3-0.2f - true;  0.2f- -> false
+    //    { 0.2f, true },  //0.2-0.1f - false; 0.1f- -> true
+    //    { 0.1f, false }, //0.1-0.0f - true;  0.0f- -> false
+    //};
 
     private void Start()
     {
@@ -22,8 +29,20 @@ public class CarExhaustEffect : MonoBehaviour
         if (ExhaustCDnow > 0f)
         {
             ExhaustCDnow -= Time.deltaTime;
-            if (ExhaustCDnow <= 0f)
+
+            //Compare blink to blink timeline
+            if (ExhaustCDnow <= 0.3f / 2 && ExhaustCDnow >= 0.2f / 2)
+                light.enabled = true;
+            else if (ExhaustCDnow <= 0.2f / 2 && ExhaustCDnow >= 0.1f / 2)
                 light.enabled = false;
+            else if (ExhaustCDnow <= 0.1f / 2 && ExhaustCDnow >= 0.0f / 2)
+                light.enabled = true;
+
+            //Unconditionally off by time it reaches zero
+            if (ExhaustCDnow <= 0f)
+            {
+                light.enabled = false;
+            }
         }
     }
 
