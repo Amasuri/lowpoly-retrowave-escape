51ff157ee192ec7cddaf18f3bdeb3146a345edb8
keiondesu96 <keiondesu96@gmail.com>
2022-09-20 02:35:59 +0400

    refactored spawn chances!

---
+++ Assets/Scripts/Entity/EnvSpawnable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvSpawnable : MonoBehaviour
+{
+    public EnvType envType;
+    public enum EnvType
+    {
+        Hut,
+        Skyscraper,
+        Bird
+    }
+
+    private void Start()
+    {
+        int chance = Random.Range(0, 100);
+
+        int spawnChance = GetSpawnChanceByType();
+
+        //Basically ~20% chance
+        if (chance >= spawnChance)
+        {
+            gameObject.SetActive(false);
+            Debug.Log(envType + " not spawned!");
+        }
+        else
+        {
+            Debug.Log(envType + " spawned!");
+        }
+    }
+
+    private int GetSpawnChanceByType()
+    {
+        switch (envType)
+        {
+            case EnvType.Hut:
+                return 20;
+
+            case EnvType.Skyscraper:
+                return 20;
+
+            case EnvType.Bird:
+                return 10;
+
+            default:
+                return 0;
+        }
+    }
+}
