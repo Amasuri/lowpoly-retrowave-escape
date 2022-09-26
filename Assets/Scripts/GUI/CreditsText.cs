cc5a3e7f9ef6a66c74fd4e3a8607dba66641656e
keiondesu96 <keiondesu96@gmail.com>
2022-09-26 11:01:55 +0400

    sort of a credits scene

---
+++ Assets/Scripts/GUI/CreditsText.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Currently doesn't do anything. The text is just a copy of text object. Might do some stuff
+/// based on it programmatically later
+/// </summary>
+public class CreditsText : MonoBehaviour
+{
+    private void Start()
+    {
+        //The game was made by:
+        //---------------------------------
+
+        //@amasumary - idea, coding, some sounds, some textures, etc
+
+        //@youkaidrawing - cars
+
+        //Assets used(Under Creative Commons):
+        //---------------------------------
+
+        //Models
+        //------
+
+        //"Low Poly Bird (Animated)"(skfb.ly / 6n6vx) by Charlie Tinley under CC BY 4.0
+
+        //"Low Poly Fisherman's Hut"(skfb.ly / 6BNHq) by RacKrab under CC BY 4.0
+
+        //"Low Poly Skyscrapers"(skfb.ly / owqyy) by exleute under CC BY 4.0
+
+        //"Palm Trees"(skfb.ly / ovANy) by Erroratten under CC BY 4.0
+
+        //Music & Sound
+        //------------ -
+
+        //Title track by Kirk Osamayo -"Shooting Star" under CC BY 4.0
+
+        //Gameplay track by HoliznaCC0 -"Back In The 80s" under CC0 1.0(public domain)
+
+        //Car pack sound pack by GGBot under CC0 1.0
+        //ggbot.itch.io/psx-style-cars
+
+        //Car exhaust by Richwise under CC0 1.0
+        //freesound.org/people/richwise/sounds/478756/
+
+        //Other
+        //-----
+
+        //Icon Pack by "Art by Kandles" Standard Unity Asset Store EULA
+
+        //assetstore.unity.com/packages/2d/gui/icons/basic-icons-139575
+
+        //-
+
+        //Thank you for playing!
+    }
+}
