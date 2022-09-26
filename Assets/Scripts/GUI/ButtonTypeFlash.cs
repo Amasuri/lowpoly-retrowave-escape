6948e813391df2314cc9d351fb70701e604ea6de
keiondesu96 <keiondesu96@gmail.com>
2022-09-26 11:42:25 +0400

    slightly clearer naming

--- Assets/Scripts/GUI/ButtonTypeFlash.cs
+++ Assets/Scripts/GUI/ButtonTypeFlash.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonFlash : MonoBehaviour, IPointerEnterHandler
+public class ButtonTypeFlash : MonoBehaviour, IPointerEnterHandler
 {
     public Button button;
     public TextMeshProUGUI terminal;
